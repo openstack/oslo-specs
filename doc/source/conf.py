89438538b3eca110b0ce6f8ce5aaa3955c778cba
Doug Hellmann <doug@doughellmann.com>
2014-09-09 09:29:22 -0400

    use yasfb for rss feeds
    Publish an RSS feed of the changes to the specs repository to make the
    specs more discoverable.
    
    Change-Id: Ib4f84efab8b3af3ddd47d38d63a91362612bd655

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,8 +24,13 @@ sys.path.insert(0, os.path.abspath('../..'))
 extensions = [
     'sphinx.ext.autodoc',
     #'sphinx.ext.intersphinx',
-    'oslosphinx'
+    'oslosphinx',
+    'yasfb',
 ]
+
+# Feed configuration for yasfb
+feed_base_url = 'http://specs.openstack.org/openstack/oslo-specs'
+feed_author = 'OpenStack Oslo Team'
 
 # Optionally allow the use of sphinxcontrib.spelling to verify the
 # spelling of the documents.
